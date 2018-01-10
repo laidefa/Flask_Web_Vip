5cab11a74e591d20f1749b8ab223948de9942c52
赖德发 <1973536419@qq.com>
2018-01-10 19:57:09 +0800

    Update hello.py

--- web_vip/hello.py
+++ web_vip/hello.py
@@ -57,11 +57,11 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    #测试调试模式
+    #测试调试模式(windows)
     #app.run(host="0.0.0.0",debug=True)
     
     
-    #线上正式模式
+    #线上正式模式(linux)
     b = True
     while (b):
         try:
